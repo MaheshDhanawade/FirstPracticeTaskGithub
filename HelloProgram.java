c51b96e47e42ad92b7c0e68d97112b26714e556e
MaheshDhanawade <81641759+MaheshDhanawade@users.noreply.github.com>
2021-03-30 22:48:19 +0530

    Meaning of  main mathod in java and printing statements was added.

--- HelloProgram.java
+++ HelloProgram.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
-class Hello
+class Hello                                                     
 {
-public static void main(String [] args)
+public static void main(String [] args)                     #Main mathod : Program verificaton start from this point
 {
-        System.out.println("Hello world..............");
+        System.out.println("Hello world..............");   #Printing Hello world
 }
 }
